6204008a575d91eaa2eac25cd377423262840912
arif123s <arif.chuty@gmail.com>
2023-12-17 11:11:23 +0600

    Route and page setup

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../Pages/Common/Includes/NavBar/NavBar";
+import Footer from "../Pages/Common/Includes/Footer/Footer";
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return (
+      <div className="text-center">
+        <h2 className="text-5xl text-green-700 font-bold text center mt-8">
+          Hello Chutyooms!!
+        </h2>
+        <button className="btn btn-outline mt-5">Start</button>
+      </div>
+    );
+};
+
+export default Home;
