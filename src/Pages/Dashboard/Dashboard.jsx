973bce06be1168d8dd850a9383dfea23b410f64c
dhiman-nath <dhiman.nath.cse@gmail.com>
2023-12-24 12:36:43 +0600

    admin dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
-import SideBar from "../Common/Includes/SideBar/SideBar";
+import SideBar from "./../Common/Includes/SideBar/SideBar";
+
+import "./Dashboard.css";
+
 
 const Dashboard = () => {
     return (
       <div>
-        <div className="flex">
+        <div className="flex dark">
           <SideBar></SideBar>
           <Outlet></Outlet>
         </div>
