babdc630ea66df551bbdf48a90f35ed5b1fefa32
Joyanta1000 <joyanta955@gmail.com>
2024-03-02 15:49:34 +0600

    Room Guest

--- src/Pages/Home/Home/HomePage/SearchField/RoomGuest.jsx
+++ src/Pages/Home/Home/HomePage/SearchField/RoomGuest.jsx
@@ -1,107 +1,147 @@
-import React from 'react';
-import { useState } from 'react';
-import Select from '@mui/material/Select';
-import './RoomGuest.css'
-import Plus from './../../../../../assets/icons/plus.svg'
-import Minus from './../../../../../assets/icons/minus.svg'
-import GreenAddIcon from './../../../../../assets/icons/Add-circle-green.svg'
-import ArrowDown from './../../../../../assets/icons/arrow-down.svg'
-
+import React from "react";
+import { useState } from "react";
+import Select from "@mui/material/Select";
+import "./RoomGuest.css";
+import Plus from "./../../../../../assets/icons/plus.svg";
+import Minus from "./../../../../../assets/icons/minus.svg";
+import GreenAddIcon from "./../../../../../assets/icons/Add-circle-green.svg";
+import ArrowDown from "./../../../../../assets/icons/arrow-down.svg";
 
 const RoomGuest = ({ isDivClicked, setDivClicked }) => {
 
+  const setDivClickedCus = (event) => {
+    event.preventDefault();
+    setDivClicked(!isDivClicked);
+  }
+  const [rooms, setRooms] = useState([
+    {
+      adults: 2,
+      children: 1,
+      child_age: {
+        0: 5,
+      },
+    },
+    {
+      adults: 2,
+      children: 2,
+      child_age: {
+        0: 2,
+        1: 1,
+      },
+    },
+    {
+      adults: 2,
+      children: 2,
+      child_age: {
+        0: 3,
+        1: 6,
+      },
+    },
+  ]);
 
-
-  const [divCount, setDivCount] = useState(1);
-  const [adultNumber, setInputValue] = useState([1]);
-  const [childNumber, setChildNumber] = useState([0]);
-
-  //select Box state
-
-  const [selectCount, setSelectCount] = useState([0]);
-  // const [selectedValues, setSelectedValues] = useState(['']);
-
-  // const handleInputChange = (event) => {
-  //   setInputValue(event.target.value);
-  // };
-
-  const addDiv = () => {
-    if (divCount < 4) {
-      setDivCount(prevCount => prevCount + 1);
-      setInputValue(prevValues => {
-        // alert(prevValues)
-        const newValues = [...prevValues]; // Create a shallow copy of the array
-        newValues[divCount] = 1; // Update the value at the specified index
-        return newValues; // Set the new array as the state
-      })
-
-      setChildNumber(prevValues => {
-        // alert(prevValues)
-        const newValues = [...prevValues]; // Create a shallow copy of the array
-        newValues[divCount] = 0; // Update the value at the specified index
-        return newValues; // Set the new array as the state
-      })
+  const removeRoom = (event, index) => {
+    // Create a copy of the rooms array
+    event.preventDefault();
+    const updatedRooms = [...rooms];
+    if(updatedRooms.length > 1){
+      // Remove the room at the specified index
+      updatedRooms.splice(index, 1);
+  
+      // Update state with the new rooms array
+      setRooms(updatedRooms);
     }
   };
 
+  const updateGuests = (event, index, type, value) => {
 
-  const updateAdultInputValue = (index, newValue) => {
-    setInputValue(prevValues => {
-      // alert(prevValues)
-      const newValues = [...prevValues]; // Create a shallow copy of the array
-      newValues[index] = newValue; // Update the value at the specified index
-      return newValues; // Set the new array as the state
-    });
+    event.preventDefault();
+    // Create a copy of the rooms array
+    const updatedRooms = [...rooms];
+
+    // Update the guests for the specified room and type (adults or children)
+    updatedRooms[index][type] = value;
+
+    // Update state with the new rooms array
+    setRooms(updatedRooms);
   };
 
-
-  const updateChildInputValue = (index, newValue) => {
-    setChildNumber(prevValues => {
-      // alert(prevValues)
-      const newValues = [...prevValues]; // Create a shallow copy of the array
-      newValues[index] = newValue; // Update the value at the specified index
-      return newValues; // Set the new array as the state
-    });
+  const addDiv = (event) => {
+    event.preventDefault();
+    const newRoom = {
+      adults: 1,  // You can set default values for adults and children as needed
+      children: 0,
+      child_age: {},  // Assuming no children initially
+    };
+  
+    // Create a copy of the current rooms array
+    const updatedRooms = [...rooms];
+  
+    // Add the new room to the array
+    updatedRooms.push(newRoom);
+  
+    // Update state with the new rooms array
+    setRooms(updatedRooms);
   };
 
-  const plusAdult = (index) => {
-    updateAdultInputValue(index, String(parseInt(adultNumber[index]) + 1));
-  }
+  const addAdultGuest = (event, index) => {
+    event.preventDefault();
+    // Create a copy of the rooms array
+    const updatedRooms = [...rooms];
 
-  const minusAdult = (index) => {
-    updateAdultInputValue(index, String(parseInt(adultNumber[index]) - 1));
-  }
+    // Increment the number of adults for the specified room
+    updatedRooms[index].adults = Math.min(updatedRooms[index].adults + 1, 3);
 
-  const plusChild = (index) => {
-    updateChildInputValue(index, String(parseInt(childNumber[index]) + 1));
-    // setSelectCount(selectCount + 1);
-    // console.log(parseInt(childNumber[index]) + 1);
-    const newChildNumber = parseInt(childNumber[index]) + 1;
-    setSelectCount(prevValues => {
-      // alert(prevValues)
-      const newValues = [...prevValues]; // Create a shallow copy of the array
-      newValues[index] = newChildNumber; // Update the value at the specified index
-      return newValues; // Set the new array as the state
-    });
-    // setSelectedValues([...selectedValues, '']);
-  }
+    // Update state with the new rooms array
+    setRooms(updatedRooms);
+  };
 
-  const minusChild = (index) => {
-    updateChildInputValue(index, String(parseInt(childNumber[index]) - 1));
+  const removeAdultGuest = (event, index) => {
+    event.preventDefault();
+    // Create a copy of the rooms array
+    const updatedRooms = [...rooms];
 
-    // setSelectCount(selectCount - 1);
-    // setSelectedValues(selectedValues.slice(0, -1));
+    // Ensure that the number of adults doesn't go below 0
+    updatedRooms[index].adults = Math.max(updatedRooms[index].adults - 1, 0);
 
-    const newChildNumber = parseInt(childNumber[index]) - 1;
-    setSelectCount(prevValues => {
-      // alert(prevValues)
-      const newValues = [...prevValues]; // Create a shallow copy of the array
-      newValues[index] = newChildNumber; // Update the value at the specified index
-      return newValues; // Set the new array as the state
-    });
+    // Update state with the new rooms array
+    setRooms(updatedRooms);
+  };
 
-  }
+  const addChildGuest = (event, index) => {
+    event.preventDefault();
+    // Create a copy of the rooms array
+    const updatedRooms = [...rooms];
 
+    // Increment the number of adults for the specified room
+    updatedRooms[index].children = Math.min(updatedRooms[index].children + 1, 2);
+
+    const newChildIndex = Object.keys(updatedRooms[index].child_age).length;
+
+    if (newChildIndex < 2 ) {
+    updatedRooms[index].child_age[newChildIndex] = 1;
+    }
+    // Update state with the new rooms array
+    setRooms(updatedRooms);
+  };
+
+  const removeChildGuest = (event, index) => {
+    event.preventDefault();
+    // Create a copy of the rooms array
+    const updatedRooms = [...rooms];
+
+    // Ensure that the number of adults doesn't go below 0
+    updatedRooms[index].children = Math.max(updatedRooms[index].children - 1, 0);
+
+    const childAgeArrayLength = Object.keys(updatedRooms[index].child_age).length;
+
+    if (childAgeArrayLength > 0 ) {
+      delete updatedRooms[index].child_age[childAgeArrayLength-1];
+      setRooms(updatedRooms);
+    }
+
+    // Update state with the new rooms array
+    setRooms(updatedRooms);
+  };
 
   const [isContentVisible, setIsContentVisible] = useState(false);
 
@@ -109,98 +149,143 @@ const RoomGuest = ({ isDivClicked, setDivClicked }) => {
     setIsContentVisible(!isContentVisible);
   };
 
-
-  // const handleSelectChange = (index, value) => {
-  //   const newSelectedValues = [...selectedValues];
-  //   newSelectedValues[index] = value;
-  //   setSelectedValues(newSelectedValues);
-  // };
-
-  const RoomGuestRemoveItem = (index) => {
-    
-    index.splice(index, 1);
-    setItems(newItems);
-  };
-
-
-
-
-
-
-
-
-
-
   return (
-
-    <div className='room-guest-content' >
-      {[...Array(divCount)].map((_, index) => (
+    <div className="room-guest-content">
+      {rooms.map((room, index) => (
         <div key={index} onClick={toggleContent}>
-          <div className='room-no-div'>
-            <div className='room-no'>Room {index + 1}</div>
-            <a type='button' className='remove-btn' onClick={() => RoomGuestRemoveItem(index)}> Remove</a>
+          <div className="room-no-div ">
+            <div className="room-no">Room {index + 1}</div>
+            <a href="#" type="button" onClick={(e) => removeRoom(e, index)} className={`remove-btn ${index === 0 ? 'invisible' : ''}`}>
+              Remove
+            </a>
           </div>
 
           <div>
-            <div className='adult-div'>
+            <div className="adult-div">
               <div>Adults</div>
-              <div className='guest-quantity-div'>
-                <a onClick={(e) => minusAdult(index)}><img className='minus-icon' src={Minus}></img></a>
-                <input className='adult-quantity' id={`adult-quantity-${index}`} type='number' value={adultNumber[index]}
-                  onChange={(e) => updateAdultInputValue(index, e.target.value)}></input>
-                <a onClick={(e) => plusAdult(index)}><img className='plus-icon' src={Plus}></img></a>
-              </div>
-            </div>
-
-            <div className='child-div'>
-              <div>Child</div>
-              <div className='guest-quantity-div'>
-                <a onClick={(e) => minusChild(index)}><img className='minus-icon' src={Minus}></img></a>
-                <input className='adult-quantity' type='number' onChange={(e) => updateChildInputValue(index, e.target.value)} value={childNumber[index]}
+              <div className="guest-quantity-div">
+                <a href="#" onClick={(e) =>
+                    removeAdultGuest(e, index)
+                    }>
+                  <img className="minus-icon" src={Minus}></img>
+                </a>
+                <input
+                  className="adult-quantity"
+                  id={`adult-quantity-${index}`}
+                  type="number"
+                  value={room.adults}
+                  onChange={(e) =>
+                    updateGuests(e, index, "adults", parseInt(e.target.value))
+                  }
                 ></input>
-                <a onClick={(e) => plusChild(index)}><img className='plus-icon' src={Plus}></img></a>
+                <a href="#" onClick={(e) =>
+                    addAdultGuest(e, index)
+                    }>
+                  <img className="plus-icon" src={Plus}></img>
+                </a>
               </div>
             </div>
-        <div className='childage-selection'>
-            {Array.from({ length: selectCount[index] }).map((_, Childindex) => (
-              <div >
-                {/* {(index == Childindex) ?
+
+            <div className="child-div">
+              <div>Child</div>
+              <div className="guest-quantity-div">
+                <a href="#" onClick={(e) =>
+                    removeChildGuest(e, index)
+                    }>
+                  <img className="minus-icon" src={Minus}></img>
+                </a>
+                <input
+                  className="adult-quantity"
+                  type="number"
+                  onChange={(e) =>
+                    updateGuests(e, index, "children", parseInt(e.target.value))
+                  }
+                  value={room.children}
+                ></input>
+                <a href="#" onClick={(e) =>
+                    addChildGuest(e, index)
+                    }>
+                  <img className="plus-icon" src={Plus}></img>
+                </a>
+              </div>
+            </div>
+            <div className="childage-selection">
+              {typeof room.child_age === "object" &&
+                Object.keys(room.child_age).map((children_age, Childindex) => (
+                  <div key={Childindex}>
+                    {/* {(index == Childindex) ?
                   ( */}
-                    <div key={Childindex} className='childage'>
-                      <select className='childage-select-box' onChange={(e) => handleSelectChange(Childindex, e.target.value)}
-                        displayEmpty >
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                      <option>6</option>
+                    <div key={Childindex} className="childage">
+                      <select
+                        className="childage-select-box"
+                        onChange={(e) =>
+                          updateGuests(
+                            index,
+                            "child_age",
+                            parseInt(e.target.value)
+                          )
+                        }
+                        // displayEmpty
+                      >
+                        <option
+                          value={1}
+                          selected={room.child_age[Childindex] === 1}
+                        >
+                          1
+                        </option>
+                        <option
+                          value={2}
+                          selected={room.child_age[Childindex] === 2}
+                        >
+                          2
+                        </option>
+                        <option
+                          value={3}
+                          selected={room.child_age[Childindex] === 3}
+                        >
+                          3
+                        </option>
+                        <option
+                          value={4}
+                          selected={room.child_age[Childindex] === 4}
+                        >
+                          4
+                        </option>
+                        <option
+                          value={5}
+                          selected={room.child_age[Childindex] === 5}
+                        >
+                          5
+                        </option>
+                        <option
+                          value={6}
+                          selected={room.child_age[Childindex] === 6}
+                        >
+                          6
+                        </option>
+                      </select>
 
-
-                    </select>
-
-                    <img className='arrow-icon-down' src={ArrowDown}></img>
+                      <img className="arrow-icon-down" src={ArrowDown}></img>
                     </div>
                     {/* ) : (<div></div>)}*/}
-              </div>
-            ))}
-
-    </div>
+                  </div>
+                ))}
+            </div>
             {/* ))} */}
           </div>
         </div>
       ))}
 
-      <div className='btn-div'>
-        <a className='addroom-btn' onClick={addDiv}>
+      <div className="btn-div">
+        <a href="#" className="addroom-btn" onClick={(e) => addDiv(e)}>
           <img src={GreenAddIcon}></img>
           <p>Add Room</p>
         </a>
-        <a onClick={() => setDivClicked(!isDivClicked)} className='done-btn'>Done</a>
+        <a href="#" onClick={(e) => setDivClickedCus(e)} className="done-btn">
+          Done
+        </a>
       </div>
     </div>
-
-
   );
 };
 
---
+++ src/Pages/Home/Home/HomePage/SearchField/RoomGuestOld.jsx
@@ -0,0 +1,208 @@
+import React from 'react';
+import { useState } from 'react';
+import Select from '@mui/material/Select';
+import './RoomGuest.css'
+import Plus from './../../../../../assets/icons/plus.svg'
+import Minus from './../../../../../assets/icons/minus.svg'
+import GreenAddIcon from './../../../../../assets/icons/Add-circle-green.svg'
+import ArrowDown from './../../../../../assets/icons/arrow-down.svg'
+
+
+const RoomGuest = ({ isDivClicked, setDivClicked }) => {
+
+
+
+  const [divCount, setDivCount] = useState(1);
+  const [adultNumber, setInputValue] = useState([1]);
+  const [childNumber, setChildNumber] = useState([0]);
+
+  //select Box state
+
+  const [selectCount, setSelectCount] = useState([0]);
+  // const [selectedValues, setSelectedValues] = useState(['']);
+
+  // const handleInputChange = (event) => {
+  //   setInputValue(event.target.value);
+  // };
+
+  const addDiv = () => {
+    if (divCount < 4) {
+      setDivCount(prevCount => prevCount + 1);
+      setInputValue(prevValues => {
+        // alert(prevValues)
+        const newValues = [...prevValues]; // Create a shallow copy of the array
+        newValues[divCount] = 1; // Update the value at the specified index
+        return newValues; // Set the new array as the state
+      })
+
+      setChildNumber(prevValues => {
+        // alert(prevValues)
+        const newValues = [...prevValues]; // Create a shallow copy of the array
+        newValues[divCount] = 0; // Update the value at the specified index
+        return newValues; // Set the new array as the state
+      })
+    }
+  };
+
+
+  const updateAdultInputValue = (index, newValue) => {
+    setInputValue(prevValues => {
+      // alert(prevValues)
+      const newValues = [...prevValues]; // Create a shallow copy of the array
+      newValues[index] = newValue; // Update the value at the specified index
+      return newValues; // Set the new array as the state
+    });
+  };
+
+
+  const updateChildInputValue = (index, newValue) => {
+    setChildNumber(prevValues => {
+      // alert(prevValues)
+      const newValues = [...prevValues]; // Create a shallow copy of the array
+      newValues[index] = newValue; // Update the value at the specified index
+      return newValues; // Set the new array as the state
+    });
+  };
+
+  const plusAdult = (index) => {
+    updateAdultInputValue(index, String(parseInt(adultNumber[index]) + 1));
+  }
+
+  const minusAdult = (index) => {
+    updateAdultInputValue(index, String(parseInt(adultNumber[index]) - 1));
+  }
+
+  const plusChild = (index) => {
+    updateChildInputValue(index, String(parseInt(childNumber[index]) + 1));
+    // setSelectCount(selectCount + 1);
+    // console.log(parseInt(childNumber[index]) + 1);
+    const newChildNumber = parseInt(childNumber[index]) + 1;
+    setSelectCount(prevValues => {
+      // alert(prevValues)
+      const newValues = [...prevValues]; // Create a shallow copy of the array
+      newValues[index] = newChildNumber; // Update the value at the specified index
+      return newValues; // Set the new array as the state
+    });
+    // setSelectedValues([...selectedValues, '']);
+  }
+
+  const minusChild = (index) => {
+    updateChildInputValue(index, String(parseInt(childNumber[index]) - 1));
+
+    // setSelectCount(selectCount - 1);
+    // setSelectedValues(selectedValues.slice(0, -1));
+
+    const newChildNumber = parseInt(childNumber[index]) - 1;
+    setSelectCount(prevValues => {
+      // alert(prevValues)
+      const newValues = [...prevValues]; // Create a shallow copy of the array
+      newValues[index] = newChildNumber; // Update the value at the specified index
+      return newValues; // Set the new array as the state
+    });
+
+  }
+
+
+  const [isContentVisible, setIsContentVisible] = useState(false);
+
+  const toggleContent = () => {
+    setIsContentVisible(!isContentVisible);
+  };
+
+
+  // const handleSelectChange = (index, value) => {
+  //   const newSelectedValues = [...selectedValues];
+  //   newSelectedValues[index] = value;
+  //   setSelectedValues(newSelectedValues);
+  // };
+
+  const RoomGuestRemoveItem = (index) => {
+    // alert(index);
+    // index.splice(index, 1);
+    // setItems(newItems);
+    console.log(divCount, 'divCount', adultNumber, 'adultNumber', childNumber, 'childNumber', selectCount, 'selectCount');
+  };
+
+
+
+
+
+
+
+
+
+
+  return (
+
+    <div className='room-guest-content' >
+      {[...Array(divCount)].map((_, index) => (
+        <div key={index} onClick={toggleContent}>
+          <div className='room-no-div'>
+            <div className='room-no'>Room {index + 1}</div>
+            <a type='button' className='remove-btn' onClick={() => RoomGuestRemoveItem(index)}> Remove</a>
+          </div>
+
+          <div>
+            <div className='adult-div'>
+              <div>Adults</div>
+              <div className='guest-quantity-div'>
+                <a onClick={(e) => minusAdult(index)}><img className='minus-icon' src={Minus}></img></a>
+                <input className='adult-quantity' id={`adult-quantity-${index}`} type='number' value={adultNumber[index]}
+                  onChange={(e) => updateAdultInputValue(index, e.target.value)}></input>
+                <a onClick={(e) => plusAdult(index)}><img className='plus-icon' src={Plus}></img></a>
+              </div>
+            </div>
+
+            <div className='child-div'>
+              <div>Child</div>
+              <div className='guest-quantity-div'>
+                <a onClick={(e) => minusChild(index)}><img className='minus-icon' src={Minus}></img></a>
+                <input className='adult-quantity' type='number' onChange={(e) => updateChildInputValue(index, e.target.value)} value={childNumber[index]}
+                ></input>
+                <a onClick={(e) => plusChild(index)}><img className='plus-icon' src={Plus}></img></a>
+              </div>
+            </div>
+        <div className='childage-selection'>
+            {Array.from({ length: selectCount[index] }).map((_, Childindex) => (
+              <div key={Childindex} >
+                {/* {(index == Childindex) ?
+                  ( */}
+                    <div key={Childindex} className='childage'>
+                      <select className='childage-select-box' onChange={(e) => handleSelectChange(Childindex, e.target.value)}
+                        displayEmpty >
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                      <option>6</option>
+
+
+                    </select>
+
+                    <img className='arrow-icon-down' src={ArrowDown}></img>
+                    </div>
+                    {/* ) : (<div></div>)}*/}
+              </div>
+            ))}
+
+    </div>
+            {/* ))} */}
+          </div>
+        </div>
+      ))}
+
+      <div className='btn-div'>
+        <a className='addroom-btn' onClick={addDiv}>
+          <img src={GreenAddIcon}></img>
+          <p>Add Room</p>
+        </a>
+        <a onClick={() => setDivClicked(!isDivClicked)} className='done-btn'>Done</a>
+      </div>
+    </div>
+
+
+  );
+};
+
+export default RoomGuest;
