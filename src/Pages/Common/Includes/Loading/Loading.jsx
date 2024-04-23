7f1808f22995a81e2853f9d82ba6945c8df9795e
arif123s <arif.chuty@gmail.com>
2024-04-23 11:43:02 +0600

    loading component update

--- src/Pages/Common/Includes/Loading/Loading.jsx
+++ src/Pages/Common/Includes/Loading/Loading.jsx
@@ -1,9 +1,21 @@
 import { CircularProgress } from "@mui/material";
+import { ThreeDots } from "react-loader-spinner";
 
 const Loading = () => {
     return (
-      <div className="min-h-screen flex mt-[250px] justify-center">
-        <CircularProgress color="success" />
+      <div className="min-h-screen flex mt-[200px] justify-center">
+        {/* <CircularProgress color="success" /> */}
+        {/* <div className="w-16 h-16 border-b-2  border-gray-900 rounded-full animate-spin"></div> */}
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
       </div>
     );
 };
