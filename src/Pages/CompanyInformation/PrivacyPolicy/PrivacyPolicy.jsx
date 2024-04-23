897dc68fd5152748e5d0c42134f1e75d9715e617
arif123s <arif.chuty@gmail.com>
2024-04-23 11:25:16 +0600

    privacy policy page with responsive

--- src/Pages/CompanyInformation/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Pages/CompanyInformation/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,101 +1,250 @@
-
 const PrivacyPolicy = () => {
-    return (
-      <div className="my-[20px] md:my-[36px] lg:my-[50px] text-[14px] md:text-[16px] lg:text-[16px] px-[8px]">
-        <div className="custom-container p-[12px] md:p-[20px] lg:p-[20px] bg-white rounded-[8px]">
-          <h2 className="text-[18px] md:text-[24px] lg:text-[24px] font-['Gilroy-semibold'] mb-[18px]">
-            Privacy Policy
+  return (
+    <div className="my-[20px] md:my-[36px] lg:my-[50px] text-[14px] md:text-[16px] lg:text-[16px] px-[8px]">
+      <div className="custom-container p-[12px] md:p-[20px] lg:p-[20px] bg-white rounded-[8px]">
+        <h2 className="text-[18px] md:text-[24px] lg:text-[24px] font-['Gilroy-semibold'] mb-[18px]">
+          Privacy Policy
+        </h2>
+        <p className="mb-[18px]">
+          The largest, most trusted, and fastest growing hospitality partner in
+          Bangladesh.
+        </p>
+        <p className="mb-[18px]">
+          Last updated:
+          <span className="text-[#020F07] font-['Gilroy-semibold']">
+            April 1, 2023
+          </span>
+        </p>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            Privacy:
           </h2>
-
-          <p className="mb-[18px]">
-            The largest, most trusted, and fastest growing hospitality partner
-            in Bangladesh.
+          <p>
+            ChutyRooms is very serious about protecting your privacy and
+            personal information. Our mission is to make it easier for everyone
+            to experience travel and to consistently exceed our guest{"'"}s
+            expectations in terms of the services we provide to our business and
+            leisure travelers. We are committed to respecting your privacy and
+            adhering to the principles of applicable data protection and
+            privacy. We have provided this Privacy Statement to explain how we
+            collect and use information collected through our website/mobile
+            app.
           </p>
-
-          <p className="mb-[18px]">
-            Last updated:
-            <span className="text-[#020F07] font-['Gilroy-semibold']">
-              April 1, 2023
-            </span>
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            Table of Contents
+          </h2>
+          <div className="flex flex-col gap-[4px]">
+            <p>1.1 Information collection</p>
+            <p>1.2 What information will we collect?</p>
+            <p>1.3 How we use personal information</p>
+            <p>1.4 With whom do we share your information?</p>
+            <p>1.5 How long we keep your data</p>
+            <p>1.6 Do not sell your data, why we don’t have it</p>
+            <p>1.7 Booking inquiries</p>
+            <p>1.8 How do we keep your personal data</p>
+            <p>1.9 The data we collect through app</p>
+            <p>1.10 What are your choices and rights</p>
+            <p>1.11 How to contact us</p>
+          </div>
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.1 Information collection
+          </h2>
+          <p>
+            It is very important that to save and protection your personal
+            information. This policy explains how we use customer’s personal
+            information by using our website www.chutyrooms.com, and smartphone
+            app such as iOS, and android, you consent to this use. We recommend
+            you review the respective privacy statements of the other parties
+            with whom you interact.
           </p>
-
-          <div className="mb-[18px]">
-            <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
-              Refund:
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.2 What information will we collect?
+          </h2>
+          <div>
+            <h2 className="font-['Gilroy-semibold']">Contact Information:</h2>
+            <div>
+              <p>1. Such as your name</p>
+              <p>2. Your address </p>
+              <p>3. Email address </p>
+              <p>4. Phone number</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-['Gilroy-semibold']">
+              Hotel Booking Information:
             </h2>
-            <div className="flex flex-col gap-[4px]">
+            <div>
+              <p>1. Check-in and check- out time</p>
+              <p>2. Number of Guests adult/child</p>
+              <p>3. Hotel name and other similar information.</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-['Gilroy-semibold']">Payment Information:</h2>
+            <p>
+              We will store your personal information in a secure and encrypted
+              format. But we do not store your credit/debit card data. All
+              credit/debit card and payment information is secure, encrypted and
+              handled by fully-certified payment providers.
+            </p>
+          </div>
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.3 How we use personal information
+          </h2>
+          <div>
+            <h2 className="font-['Gilroy-semibold']">
+              We may also use your personal information for several reasons
+            </h2>
+            <div>
               <p>
-                1. ChutyRooms will refund your full payment if you cancel your
-                booking before
-                <span className="text-[#020F07] font-['Gilroy-semibold']">
-                  7 days.
-                </span>
+                1. Confirm your reservations with the respective service
+                providers.
               </p>
               <p>
-                2. You must pay a fixed amount of charges if you cancel the
-                booking before
-                <span className="text-[#020F07] font-['Gilroy-semibold']">
-                  72 hours
-                </span>
-                , as per hotel policy.
+                2. We collect and analyze users’ personal information in order
+                to provide a better experience to all users.
               </p>
               <p>
-                3. Customers must pay full payment if you cancel the booking
-                before
-                <span className="text-[#020F07] font-['Gilroy-semibold']">
-                  24 hours
-                </span>
-                , as per hotel policy.
+                3. Inform and keep our users up to date with our latest offers
+                and services.
+              </p>
+              <p>4. Send a verification message or email</p>
+              <p>
+                5. Validate and authenticate your account to prevent any misuse
+                or abuse.
               </p>
               <p>
-                4. If the child is
-                <span className="text-[#020F07] font-['Gilroy-semibold']">
-                  aged 6–10
-                </span>
-                , you must pay a half payment, and if the child is aged 10+ you
-                just need an extra bed and must pay an extra payment as per the
-                hotel policy.
-              </p>
-              <p>
-                5. Refund and cancellation policies depend on individual hotel
-                terms and conditions.
-              </p>
-              <p>
-                6. For extra persons, additional rooms, extra beds, and a
-                swimming pool, the charge depends on hotel policy.
-              </p>
-              <p>
-                7. If you cancel before the hotel{"'"}s cancellation deadline
-                but still see a charge from them on your credit card statement,
-                get in touch with the payment provider or ChutyRooms to make
-                sure you get your refund.
-              </p>
-              <p>
-                8. An amendment to the arrival date depends on the availability
-                of the desired arrival date.
-              </p>
-              <p>
-                9. Generally, two adults can stay in a room; if an additional
-                person wants to stay with them, they must bear the additional
-                cost as per hotel policy and the ChutyRooms policy.
-              </p>
-              <p>
-                10. Customers must go to my booking cancel to cancel their
-                reservation.
-              </p>
-              <p>
-                11. After successful cancellation, your refund will be returned
-                within
-                <span className="text-[#020F07] font-['Gilroy-semibold']">
-                  7 to 10 working days
-                </span>
-                , maximum.
+                6. Contact you on your birthday or anniversary to offer a
+                special gift or offer.
               </p>
             </div>
           </div>
         </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.4 Who do we share your information with?
+          </h2>
+          <div>
+            <h2 className="font-['Gilroy-semibold']">
+              Legal compliance and obligations
+            </h2>
+            <p>
+              We may need to share your information for any legal assistance,
+              investigation, compliance, regulatory, or audit purpose.
+            </p>
+          </div>
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.5 Bookings and booking inquiries
+          </h2>
+          <div>
+            <h2 className="font-['Gilroy-semibold']">
+              When you make a booking, we collect the following data:
+            </h2>
+            <div>
+              <p>1. Check-in and check-out times and dates</p>
+              <p>2. Your first and last names</p>
+              <p>3. The number of guests</p>
+              <p>4. Age range</p>
+              <p>5. Your country</p>
+              <p>6. Your email address</p>
+              <p>7. Your phone number</p>
+              <p>8. Hotel name and other similar information</p>
+            </div>
+          </div>
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.6 How do we keep your personal data secure?
+          </h2>
+          <p>
+            Our highest priority is to secure your personal data. ChutyRooms
+            organizes an expert IT team to keep your personal data as safe as
+            possible.
+          </p>
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.7 How long do we keep your data?
+          </h2>
+          <p>
+            Generally, where we have a contractual relationship with you, we
+            will retain your data for a period of a lifetime from the date our
+            contractual relationship ends. If necessary for the purpose of
+            further legal investigation.
+          </p>
+        </div>
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.8 Children’s Privacy
+          </h2>
+          <p>
+            Protecting the privacy of young children is especially important for
+            ChutyRooms. For that reason, ChutyRooms does not collect or solicit
+            personal information from anyone under the age of 18. If he or she
+            books a hotel by using the ChutyRooms website or apps, If you are
+            under 18, please send us your information about yourself to
+            chutyrooms.com. Include your name, address, telephone number, email,
+            partners, ID/passport, and driving license.
+          </p>
+        </div>
+
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.9 The data we collect through app
+          </h2>
+          <div>
+            <h2 className="font-['Gilroy-semibold']">
+              When you install the Chutyrooms app, we collect the following
+              data:
+            </h2>
+            <div>
+              <p>
+                1. The information you get to enter while using our services
+              </p>
+              <p>
+                2. Personal information like name, email address, country code,
+                city, phone number, and personal address
+              </p>
+              <p>
+                3. Reservations information like check-in and check-out dates,
+                guest numbers, hotel names, and other similar information
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.10 What are your choices and rights
+          </h2>
+          <p>
+            If you have any questions about ChutyRooms, please feel free to
+            contact us.
+          </p>
+        </div>
+
+        <div className="mb-[18px]">
+          <h2 className="text-[16px] md:text-[18px] lg:text-[18px] font-['Gilroy-semibold'] mb-[4px]">
+            1.11 How to contact us
+          </h2>
+          <p>
+            If you have any questions about ChutyRooms, please feel free to
+            contact us.
+          </p>
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default PrivacyPolicy;
